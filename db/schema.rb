6d965674f8b5ebc4ec7e32d4011fe80ec2da12c8
Jared Levin <jaredrlevin@gmail.com>
2016-05-11 11:51:58 -0700

    added initial styling

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160509151825) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "searches", force: :cascade do |t|
     t.string   "query"
     t.datetime "created_at", null: false
